1772a93abc4db50fd67bbc527fe091894ea0fc58
Geoff 👌  Cowan <geoffroy.cowan@gmail.com>
2021-07-05 20:06:41 +1000

    more

--- index.ts
+++ index.ts
@@ -1,8 +1,13 @@
 import { request } from "@octokit/request";
 
-const token = `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`;
+const tokenKey = "token_storage_key";
 
 async function go() {
+  let token = localStorage.getItem(tokenKey);
+  if (!token) {
+    token = window.prompt("token pls");
+    localStorage.setItem(tokenKey, token);
+  }
   const getResult = await request(
     "GET /repos/tomatosource/todo/contents/todo.md",
     {
@@ -12,21 +17,29 @@ async function go() {
     }
   );
   let raw = atob(getResult.data.content);
-  console.log(raw);
-  raw += "yep";
-  console.log(raw);
-  console.log(getResult);
-  const result = await request(
-    "PUT /repos/tomatosource/todo/contents/todo.md",
-    {
-      headers: {
-        authorization: `token ${token}`,
-      },
-      message: "update todo.md",
-      content: btoa(raw),
-      sha: getResult.data.sha,
+
+  const textAreaEle = document.getElementById(
+    "textarea"
+  ) as HTMLTextAreaElement;
+  textAreaEle.textContent = raw;
+
+  let lastSaved = raw;
+  setInterval(async () => {
+    const newVal = textAreaEle.value;
+    console.log(newVal);
+    if (newVal != lastSaved) {
+      console.log("saving");
+      await request("PUT /repos/tomatosource/todo/contents/todo.md", {
+        headers: {
+          authorization: `token ${token}`,
+        },
+        message: "update todo.md",
+        content: btoa(raw),
+        sha: getResult.data.sha,
+      });
+      lastSaved = newVal;
     }
-  );
+  }, 5000);
 }
 
 go();
